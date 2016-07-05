a7c43676375b9f0022ca28596986eb059d15fb30
lodycas <lodycas@gmail.com>
2016-07-05 11:49:01 +0200

    Add files via upload

---
+++ M1polynome/student/Exercice2Vide.java
@@ -0,0 +1,18 @@
+package student;
+
+public class Exercice2Stu {
+
+    // Soluce
+    public static int[] prodAlgeb(int a, int b, int c){
+
+        int xCube = 0;
+        int xCarre = 0;
+        int x = 0;
+        int tI = 0;
+        
+        @    @q1@@
+        
+        int i[] = {xCube, xCarre, x, tI};
+        return i;
+        }
+}
