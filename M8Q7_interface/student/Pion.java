cba48511038a826f7c41d1ea7e190ca134881a60
Brandonbn <brandon.naitali@student.uclouvain.be>
2016-07-05 17:03:55 +0200

    nterface amélioration

--- M8Q7_interface/student/Pion.java
+++ M8Q7_interface/student/Pion.java
@@ -5,6 +5,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.Test;
 import java.util.Random;
+import java.lang.reflect.Constructor;
 import org.junit.runner.notification.Failure;
 
 public class Pion {
@@ -13,13 +14,34 @@ public class Pion {
 	
 	@Test
 	public void testPos(){
+    	PionStu pq=new PionStu();
 		try{
-			PionStu p = new PionStu("blanc");
-            assertEquals(42,p.seDeplacer(),0);
-            assertEquals(43,p.estMangee(null),0);
-            assertEquals(44,p.mangePiece(null),0);
-			//assertEquals("blanc",p.couleurPiece(),0);
-            //assertEquals("pion",p.nomPiece(),0);
+        	
+        	Class<?> clazz = PionStu.MyPion.class;
+            if(!ChessPiece.class.isAssignableFrom(mypion.getClass())){
+            	fail(str + "Vous n'implémentez pas l'interface ChessPiece");
+            }
+            
+        	PionStu ps=new PionStu();
+            String r="blanc";
+			PionStu.MyPion p = ps.new MyPion(r);
+            
+            if( p.seDeplacer()!= (PionStu.temp)+1){
+            	fail(str + "Vous n'avez pas correctement implémenté seDeplacer()");
+            }
+            //assertEquals(PionStu.temp,0,0);
+            if( p.estMangee(null)!=(PionStu.temp)+2){
+            	fail(str + "Vous n'avez pas correctement implémenté estMangee()");
+            }
+            if( p.mangePiece(null)!=(PionStu.temp)+3){
+            	fail(str + "Vous n'avez pas correctement implémenté mangePiece()");
+            }
+            if(! (p.couleurPiece().equals("blanc"))){
+            	fail(str + "Vous n'avez pas correctement implémenté couleurPiece()");
+            }
+            if(! (p.nomPiece().equals("pion"))){
+            	fail(str + "Vous n'avez pas correctement implémenté nomPiece()");
+            }
             
             
             
@@ -40,7 +62,11 @@ public class Pion {
 		}catch(NullPointerException e){
 			fail(str + "Attention, vous faites une opération sur un objet qui vaut null ! Veillez à bien gérer ce cas.");
 			e.printStackTrace();
-		}catch(Exception e){
+		}catch(ClassNotFoundException e){
+			fail(str + "Mauvaise classe!");
+			e.printStackTrace();
+        
+        }catch(Exception e){
 			fail(str + "\n" + e.getMessage());
 			e.printStackTrace();
 		}
