cdc1b84dd37b85275a3cc27f7516e5f6d4596e05
Justine Doutreloux <justine@wifi-student6-508.sri.ucl.ac.be>
2016-07-07 14:36:33 +0200

    chgt nom dossier

---
+++ OWM9_3/student/M9Q3.java
@@ -0,0 +1,36 @@
+package student;
+/**
+ *  Copyright (c) 2016 Justine Doutreloux, Carolina Unriza, Charline Outters
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.junit.Assert.*;
+import java.io.*;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.Test;
+import java.util.Random;
+import org.junit.runner.notification.*;
+public class M9Q3 
+{
+	private static String str = "Le code semble comporter des erreurs : ";
+	
+	@Test
+	public void test()
+	{
+		int res = M9Q3Stu.charNumber("fichier.txt");
+		int res1 = 
+		assertEquals(res, res1);
+	}
+	
+}
