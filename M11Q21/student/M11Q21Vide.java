c7a3a971ef6616ada6ee8bc5749149237cda45f9
Alexandre Kühn <alex.e.kuhn@gmail.com>
2016-07-08 12:49:03 +0200

    Mission 11: update (manque images et fix names)

---
+++ M11Q21/student/M11Q21Vide.java
@@ -0,0 +1,40 @@
+package student;
+
+public class M11Q21Stu {
+
+    public class SList {
+
+        public SNode first;
+
+        public SList() {
+            this.first = null; // empty list
+        }
+
+        public String toString() {
+            String res = "";
+            if (this.first == null)
+                return "empty SList";
+            SNode current = this.first;
+            while(current != null) {
+                res += current.value;
+                current = current.next;
+                if (current != null) {
+                    res += " -> ";
+                }
+            }
+            return res;
+        }
+
+        @@q1@@
+    }
+
+    public class SNode {
+        public int value;
+        public SNode next;
+
+        public SNode(int v) {
+            this.value = v;
+            this.next = null;
+        }
+    }
+}
