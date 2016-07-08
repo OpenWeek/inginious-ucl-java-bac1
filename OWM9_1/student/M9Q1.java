2aa92e14e05d526a6b5df8749745982902a7c431
lin3out <charline.outters@gmail.com>
2016-07-08 15:08:56 +0200

    Commenting of the code

--- OWM9_1/student/M9Q1.java
+++ OWM9_1/student/M9Q1.java
@@ -12,6 +12,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
 package student;
 import static org.junit.Assert.*;
 import java.io.*;
@@ -26,47 +27,50 @@ public class M9Q1 {
 	
 	private static String str = "Le code semble comporter des erreurs : ";
 	private static String writed;
+	
+	//Generates a random String to write in the file (to avoid students getting the answer and just print it)
 	public static String generateString(int length){
 		String s = "";
 		Random r = new Random();
 		for(int i = 0 ; i < length ; i++){
 			s += (char) ((r.nextInt('z' - 'a') + 'a'));
 		}
-        writed = s;
+	        writed = s;
 		return s;
 	}
     
+    	//Writes the generated String in the file
 	public static void writeFile(){
         String toW = generateString(6);
 		try{
 			PrintWriter pw = new PrintWriter(new FileWriter("student/fichier.txt"));
 			pw.println(toW);
 			pw.close();
-        }
-        catch(IOException e){
+	 	}catch(IOException e){
 			e.printStackTrace();
-		}
-    }
+	 	}
+    	}
     
    
 	@Test
+	//Tests the correct opening of the file
+	//Compares the answer given by the student with the text in the file
 	public void testPos(){
-    	writeFile();
+    		writeFile();
 		try{
-            M9Q1Stu.premierFichier("student/fichier.txt");
-            BufferedReader stuFile;
-            stuFile = new BufferedReader(new FileReader("student/outStu.txt"));
-            String res = stuFile.readLine();
-            stuFile.close();
-            assertEquals(writed, res);
-			}
-		catch(FileNotFoundException e){
+	            M9Q1Stu.premierFichier("student/fichier.txt");
+	            BufferedReader stuFile;
+	            stuFile = new BufferedReader(new FileReader("student/outStu.txt"));
+	            String res = stuFile.readLine();
+	            stuFile.close();
+	            assertEquals(writed, res);
+		}catch(FileNotFoundException e){
 			fail(str + "Le code est incorrect : le nom du fichier ne semble pas être correct, vous essayez d'ouvrir un fichier non existant.");
 			e.printStackTrace();
 		}catch(IOException e){
 			fail(str + "Le code est incorrect: il y a un problème lors de l'utilisation de la méthode readLine() ou close()");
-			e.printStackTrace();}
-		catch(IllegalArgumentException  e){
+			e.printStackTrace();
+		}catch(IllegalArgumentException  e){
 			e.printStackTrace();
 			fail(str + "Attention, vous tentez de lire en dehors des limites d'un String ! (StringIndexOutOfBoundsException)");
 			e.printStackTrace();
@@ -75,7 +79,7 @@ public class M9Q1 {
 	
 
 	
-	// Code verificateur
+	// Checking code
 	public static void main(String[] args) {
 		Result result = JUnitCore.runClasses(M9Q1.class);
 		for (Failure failure: result.getFailures()) {
