689dd52fd1cdff00461d38239e10da6c1ebaa3d2
Spirou07 <florian.fitvoye@student.uclouvain.be>
2016-07-08 10:57:14 +0200

    SW interface

---
+++ OW_SW_REVISION/student/Professeur.java
@@ -0,0 +1,16 @@
+public class Professeur extends Personne{
+  String name;
+  int age;
+  boolean seen;
+  boolean spoil;
+  public Professeur (String n, int a, boolean s, boolean sp){
+    this.name=n;
+    this.age=a;
+    this.seen=s;
+    this.spoil=sp;
+  }
+  public String getName(){return this.name;}
+  public int getAge(){return this.age;}
+  public boolean hasSeenStarWars(){return this.seen;}
+  public boolean spoilerAlert(){return this.spoil;}
+}
